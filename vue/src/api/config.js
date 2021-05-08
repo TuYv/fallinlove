6713a63cb9105fe63c944f8e1633217782062f90
max <861506831@qq.com>
2021-05-08 16:55:40 +0800

    前端-登录功能

--- vue/src/api/config.js
+++ vue/src/api/config.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const service = axios.create({
     baseURL: 'http://localhost:8082/',
     // 请求超时时间
-    timeout: 3000
+    timeout: 30000
 })
 
 //请求拦截器
--- vue/src/components/Login.vue
+++ vue/src/components/Login.vue
@@ -2,18 +2,21 @@
     <div class="login" clearfix>
         <div class="login-wrap">
             <el-row type="flow" justify="center">
-              <el-col ref="loginForm" :model="user" :rules="rules" status-icon label-width="80px">
+              <el-form ref="loginForm" :model="user" :rules="rules" status-icon label-width="80px">
                   <h3>登陆</h3>
                   <hr>
                   <el-form-item label="用户名" prop="username">
-                      <el-input v-model="user.username" placeholder="请输入用户名" prefix-icon />
+                      <el-input v-model="user.username" placeholder="请输入用户名" maxlength="12" prefix-icon />
                   </el-form-item>
                   <el-form-item label="密码" prop="password" id="password">
                       <el-input v-model="user.password" show-password placeholder="请输入密码" prefix-icon />
                   </el-form-item>
                   <router-link to="/">找回密码</router-link>
                   <router-link to="/register">注册账号</router-link>
-              </el-col>
+                  <el-form-item>
+                    <el-button type="primary" icon="el-icon-upload" @click="doLogin()">登 录</el-button>
+                  </el-form-item>
+              </el-form>
             </el-row>
         </div>
     </div>
@@ -21,12 +24,13 @@
 <script>
 export default {
     name: "login",
-    date() {
+    data() {
         return {
             user: {
                 username: "",
                 password: ""
-            }
+            },
+            rules:{username:['^([A-Z]|[a-z]|[0-9]|[`-=[];,./~!@#$%^*()_+}{:?]){6,20}$']}
         };
     },
     created() {},
@@ -49,12 +53,12 @@ export default {
 .login {
   width: 100%;
   height: 740px;
-  background: url("../assets/images/bg1.png") no-repeat;
+  background: url("../assets/images/bg1.jpg") no-repeat;
   background-size: cover;
   overflow: hidden;
 }
 .login-wrap {
-  background: url("../assets/images/login_bg.png") no-repeat;
+  background: url("../assets/images/login_bg.jpg") no-repeat;
   background-size: cover;
   width: 400px;
   height: 300px;
