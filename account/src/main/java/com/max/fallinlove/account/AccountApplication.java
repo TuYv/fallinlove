9590bce10b739276daa4d24bf5ea1f9edb922c85
max <861506831@qq.com>
2021-05-18 22:14:53 +0800

    nacos服务注册配置

--- account/src/main/java/com/max/fallinlove/account/AccountApplication.java
+++ account/src/main/java/com/max/fallinlove/account/AccountApplication.java
@@ -3,7 +3,9 @@ package com.max.fallinlove.account;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.max.fallinlove.account.mapper")
 public class AccountApplication {
