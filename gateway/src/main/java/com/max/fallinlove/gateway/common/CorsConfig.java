82bb3c52a487d1d2127a8f4fa7fd1b122f2c3b58
max <861506831@qq.com>
2021-06-17 17:48:35 +0800

    跨域

---
+++ gateway/src/main/java/com/max/fallinlove/gateway/common/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.max.fallinlove.gateway.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        //允许所有请求头
+        config.addAllowedMethod("*");
+        //允许所有请求方法，例如get，post等
+        config.addAllowedOrigin("*");
+        //允许所有的请求来源
+        config.addAllowedHeader("*");
+        //允许携带cookie
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        //对所有经过网关的请求都生效
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
