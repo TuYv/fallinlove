607744a6d790d3d1467f665e195edd428d3116b4
Rick <861506831@qq.com>
2022-02-16 22:19:39 +0800

    增加一个非空校验

--- finance/src/test/java/com/max/fallinlove/finance/FinanceApplicationTests.java
+++ finance/src/test/java/com/max/fallinlove/finance/FinanceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.max.fallinlove.finance;
 
+import com.max.fallinlove.common.tools.RedisUtils;
 import com.max.fallinlove.finance.repository.MonthAmountRepository;
 import com.max.fallinlove.finance.service.impl.MonthAmountDetailServiceImpl;
 import javax.annotation.Resource;
@@ -11,10 +12,14 @@ class FinanceApplicationTests {
 
     @Resource
     private MonthAmountRepository monthAmountRepository;
+    @Resource
+    private RedisUtils redisUtils;
     @Resource private MonthAmountDetailServiceImpl monthAmountDetailServiceImpl;
     @Test
     void contextLoads() {
-        System.out.println(monthAmountDetailServiceImpl.getMonthTagAmountList(1));
+        //redisUtils.set("test", "234");
+        System.out.println(redisUtils.rangeZset("2",0, -1));
+        //System.out.println(monthAmountDetailServiceImpl.getMonthTagAmountList(1));
     }
 
 }
